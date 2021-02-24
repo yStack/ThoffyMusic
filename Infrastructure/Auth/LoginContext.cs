6ce788e44c807673c28e62fed96f415158a1cfaa
ystack <vonfat@gmail.com>
2021-02-25 00:12:59 +0800

    初始化

---
+++ Infrastructure/Auth/LoginContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    /// <summary>
+    /// 策略模式Context类
+    /// </summary>
+    class LoginContext
+    {
+        //登录策略
+        private ILoginStrategy _loginStrategy;
+
+        //user对象
+        private User _user;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="loginStrategy"></param>
+        /// <param name="user"></param>
+        public LoginContext(ILoginStrategy loginStrategy, User user)
+        {
+            _loginStrategy = loginStrategy;
+            _user = user;
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="isQrLogin"></param>
+        /// <returns></returns>
+        public bool Login(User user, bool isQrLogin)
+        {
+            return _loginStrategy.Login(user.Email, user.CellPhone, isQrLogin, user.Password);
+        }
+
+    }
+
+    interface ILoginStrategy
+    {
+        /// <summary>
+        /// 用户登录的方法接口
+        /// </summary>
+        /// <param name="email">email地址</param>
+        /// <param name="cellPhone">手机号码</param>
+        /// <param name="qrCode"></param>
+        /// <param name="password">密码</param>
+        /// <returns></returns>
+        bool Login(string email, string cellPhone, bool isQrLogin, string password);
+    }
+
+
+    public class EmailLogin : ILoginStrategy
+    {
+        public bool Login(string email, string cellPhone, bool isQrLogin, string password)
+        {
+            // 检查输入是否合法
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password)) return false;
+
+            string timeStamp = UrlHelper.GetTimeStamp().ToString();
+            var url = UrlHelper.RootUrl + $"/login?timeStamp={timeStamp}";
+           
+            
+            return false;
+        }
+    }
+
+
+
+
+}
---
+++ Infrastructure/UrlHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Security.Cryptography;
+
+
+namespace Infrastructure
+{
+    class UrlHelper
+    {
+        public static readonly string RootUrl = "http://localhost:3000";
+
+        /// <summary>
+        /// GET方法
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string Get(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string encoding = response.ContentEncoding;
+            if (encoding == null || encoding.Length < 1)
+            {
+                encoding = "UTF-8"; //默认编码  
+            }
+
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(encoding));
+            string retString = reader.ReadToEnd();
+            return retString;
+        }
+
+        /// <summary>
+        /// POST 方法
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="jsonStr"></param>
+        /// <returns></returns>
+        public static string Post(string url, string jsonStr)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "POST";
+            req.ContentType = "application/json";
+            req.Timeout = 5000;
+
+            // 
+            byte[] data = Encoding.UTF8.GetBytes(jsonStr);
+            using (Stream reqStream = req.GetRequestStream())
+            {
+                reqStream.Write(data, 0, data.Length);
+            }
+
+            // 获取返回值
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            using (StreamReader reader = new StreamReader(resp.GetResponseStream(), Encoding.UTF8))
+            {
+                string respStr = reader.ReadToEnd();
+                return respStr;
+            }
+        }
+
+        /// <summary>
+        /// 当前时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static int GetTimeStamp()
+        {
+            var startTime = TimeZoneInfo.ConvertTime(new DateTime(1970, 1, 1), TimeZoneInfo.Local);
+            return (int)(DateTime.Now - startTime).TotalSeconds;
+        }
+
+        /// <summary>
+        /// 字符串转小写32位MD5字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string Md5(string str)
+        {
+            var md5 = MD5.Create();
+            var hashByte = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+            var hashByte2Str = BitConverter.ToString(hashByte);
+            var result = hashByte2Str.Replace("-", "").ToLower();
+            md5.Dispose();
+            return result;
+        }
+    }
+}
